4e1696e53ac8d3d1f4ba2b3979fadc0d48a7f540
David Ball <david@daball.me>
2013-11-10 22:14:11 -0500

    Update tests

--- tests.js
+++ tests.js
@@ -13,6 +13,12 @@ address.City = 'Cleveland';
 address.State = 'Virginia';
 address.PostalCode = '24225';
 
-console.log('Address Object:', address);
-console.log('Address Label:');
-console.log(address.toString());
+console.log('Test 1: Author Address Label');
+console.log(address.toUSPSString(), '\n');
+
+//try to fix a misspelled address line
+console.log('Test 2: Misspelled Street Name Suffix Label');
+address = new usps.AddressBuilder();
+address.StreetNumber = '1';
+address.StreetName = 'Misspelled Drv';
+console.log(address.toUSPSString());
